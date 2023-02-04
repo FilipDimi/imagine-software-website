9ad96dd71bc9ffd5d5c6ea6c8351fe2b02b8c751
dimitr36 <dimitr36@msu.edu>
2023-02-04 16:01:09 -0500

    Add image to the header container

--- src/App.js
+++ src/App.js
@@ -1,71 +1,10 @@
-import { useState } from "react";
-import { BulbFilled, FileFilled } from "@ant-design/icons";
-import logo from "./assets/icon.png";
-import styles from "./App.module.css";
+import NavigationBar from "./components/UI/NavigationBar";
 
 function App() {
-  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
-
-  const showBurgerManuHandler = () => {
-    setShowBurgerMenu(!showBurgerMenu);
-  };
-
   return (
-    <div className={styles.mainHeaderContainer}>
-      <header className={styles.appHeader}>
-        <nav className={styles.fullNavBar}>
-          <ul className={styles.navBarItemsContainer}>
-            <li>
-              <img src={logo} alt="Imagine Software" width="65px" />
-            </li>
-            <li className={styles.horizontalLine}></li>
-            <li className={styles.fullNavBarItem}>Home</li>
-            <li className={styles.fullNavBarItem}>About</li>
-            <li className={styles.fullNavBarItem}>Contact</li>
-            <li className={styles.fullNavBarItem}>Team</li>
-          </ul>
-          <ul className={styles.navBarItemsContainer}>
-            <li className={styles.horizontalLine}></li>
-            <li className={styles.fullNavBarItem}>
-              <BulbFilled className={styles.icon} />
-            </li>
-            <li className={styles.fullNavBarItem}>
-              <FileFilled className={styles.icon} />
-            </li>
-            <li className={styles.fullNavBarItem}> </li>
-          </ul>
-
-          <ul className={styles.burgerNavBarButtons}>
-            <li>
-              <h3>Imagine Software</h3>
-            </li>
-            <li>
-              <button
-                className={styles.burgerButton}
-                onClick={showBurgerManuHandler}
-              >
-                =
-              </button>
-            </li>
-          </ul>
-        </nav>
-
-        {showBurgerMenu && (
-          <ul className={styles.burgerNavBar}>
-            <li className={styles.burgerNavItem}>Home</li>
-            <li className={styles.burgerNavItem}>About</li>
-            <li className={styles.burgerNavItem}>Contact</li>
-            <li className={styles.burgerNavItem}>Team</li>
-            <li className={styles.burgerNavItem}>
-              <BulbFilled className={styles.icon} />
-            </li>
-            <li className={styles.burgerNavItem}>
-              <FileFilled className={styles.icon} />
-            </li>
-          </ul>
-        )}
-      </header>
-    </div>
+    <>
+      <NavigationBar />
+    </>
   );
 }
 
---
+++ src/components/UI/NavigationBar.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { BulbFilled, FileFilled } from "@ant-design/icons";
+import styles from "./NavigationBar.module.css";
+
+const NavigationBar = () => {
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+
+  const showBurgerManuHandler = () => {
+    setShowBurgerMenu(!showBurgerMenu);
+  };
+  return (
+    <div className={styles.mainHeaderContainer}>
+      <header className={styles.appHeader}>
+        <nav className={styles.fullNavBar}>
+          <ul className={styles.navBarItemsContainer}>
+            <li className={styles.fullNavBarItem}>Home</li>
+            <li className={styles.fullNavBarItem}>About</li>
+            <li className={styles.fullNavBarItem}>Contact</li>
+            <li className={styles.fullNavBarItem}>Team</li>
+          </ul>
+          <ul className={styles.navBarItemsContainer}>
+            <li className={styles.horizontalLine}></li>
+            <li className={styles.fullNavBarItem}>
+              <BulbFilled className={styles.icon} />
+            </li>
+            <li className={styles.fullNavBarItem}>
+              <FileFilled className={styles.icon} />
+            </li>
+            <li className={styles.fullNavBarItem}> </li>
+          </ul>
+
+          <ul className={styles.burgerNavBarButtons}>
+            <li>
+              <h3>Imagine Software</h3>
+            </li>
+            <li>
+              <button
+                className={styles.burgerButton}
+                onClick={showBurgerManuHandler}
+              >
+                =
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+        {showBurgerMenu && (
+          <ul className={styles.burgerNavBar}>
+            <li className={styles.burgerNavItem}>Home</li>
+            <li className={styles.burgerNavItem}>About</li>
+            <li className={styles.burgerNavItem}>Contact</li>
+            <li className={styles.burgerNavItem}>Team</li>
+            <li className={styles.burgerNavItem}>
+              <BulbFilled className={styles.icon} />
+            </li>
+            <li className={styles.burgerNavItem}>
+              <FileFilled className={styles.icon} />
+            </li>
+          </ul>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default NavigationBar;
